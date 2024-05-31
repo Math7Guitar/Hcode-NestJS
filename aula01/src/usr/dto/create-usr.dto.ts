263ea82ef0f98258a23c7c5df48f6b561ea4b397
Matt <58275953+Math7Guitar@users.noreply.github.com>
2024-05-31 18:41:26 -0300

    Delete aula01 directory

--- aula01/src/usr/dto/create-usr.dto.ts
+++
@@ -1,8 +0,0 @@
-import { UUID } from 'crypto';
-
-export class CreateUsrDto {
-  id: UUID;
-  name: string;
-  username: string;
-  password: string;
-}
--- aula01/src/usr/dto/update-usr.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUsrDto } from './create-usr.dto';
-
-export class UpdateUsrDto extends PartialType(CreateUsrDto) {}
